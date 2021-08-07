af25b8724a15263b9336cab93ba78e98a5cf05c3
jaywcjlove <398188662@qq.com>
2021-08-08 00:27:00 +0800

    fix: Update the android example of HelloWorld.

--- HelloWorld/.prettierrc.js
+++ HelloWorld/.prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
+  tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  printWidth: 160,
 };
