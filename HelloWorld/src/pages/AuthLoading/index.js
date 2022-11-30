63d154e43ee567baa2d437abd5331d143615f1b6
Chenling <42308734@qq.com>
2022-11-30 15:44:37 +0800

    deps: 增加react-query请求 (#230)
    * chore: update react-native0.69.3 to react-native0.70.5
    
    * doc: update README
    
    * fix: 修复android运行报错
    
    * released v5.0.0
    
    * chore: update react-native0.70.5 to react-native0.70.6
    
    * chore: 升级ios/android配置
    
    * released v5.1.0
    
    * fix(deps): 增加react-query请求

--- HelloWorld/src/pages/AuthLoading/index.js
+++ HelloWorld/src/pages/AuthLoading/index.js
@@ -1,52 +1,55 @@
-import React from 'react';
-import {Text, StatusBar, StyleSheet, SafeAreaView} from 'react-native';
-import {connect} from 'react-redux';
-import {Flex, Loader, H3, Icon} from '@uiw/react-native';
+import React, { useEffect } from 'react';
+import { Text, StatusBar, StyleSheet, SafeAreaView } from 'react-native';
+import { connect } from 'react-redux';
+import { Flex, Loader, H3, Icon } from '@uiw/react-native';
 import Global from '../../global';
-import {logoLight} from '../../components/icons/signin';
+import { logoLight } from '../../components/icons/signin';
 import Footer from '../../components/Footer';
+import { authToken } from '../../hooks/users'
 
-class AuthLoadingScreen extends React.Component {
-  componentDidMount() {
-    const {navigation, authToken} = this.props;
+const AuthLoadingScreen = ({
+  navigation,
+  update,
+  token,
+  authState,
+  children
+}) => {
+  const { mutate, isLoading } = authToken({ update, token })
+  useEffect(() => {
     if (navigation && Global) {
       Global.navigation = navigation;
     }
-    authToken();
+    mutate();
+  }, [])
+
+  if (children && typeof children === 'function' && authState) {
+    return children(token);
   }
-  render() {
-    const {token, loading, authState, children} = this.props;
-    if (children && typeof children === 'function' && authState) {
-      return children(token);
-    }
-    return (
-      <SafeAreaView style={styles.container}>
-        <Flex direction="column" justify="center" align="center" style={{flex: 1}}>
-          <StatusBar barStyle="light-content" />
-          <Flex justify="center" align="center" direction="column" style={styles.header}>
-            <Icon xml={logoLight} size={75} />
-            <H3 style={styles.title}>My APP</H3>
-          </Flex>
-          <Flex style={{height: 32, flex: 1, width: '100%'}}>
-            <Loader loading={loading} maskColor="transtion" vertical rounded={5} tip={<Text style={{color: '#fff', marginTop: 15}}>Verify login...</Text>} />
-          </Flex>
-          <Footer style={{marginBottom: 20}} />
+  return (
+    <SafeAreaView style={styles.container}>
+      <Flex direction="column" justify="center" align="center" style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <Flex justify="center" align="center" direction="column" style={styles.header}>
+          <Icon xml={logoLight} size={75} />
+          <H3 style={styles.title}>My APP</H3>
         </Flex>
-      </SafeAreaView>
-    );
-  }
+        <Flex style={{ height: 32, flex: 1, width: '100%' }}>
+          <Loader loading={isLoading} maskColor="transtion" vertical rounded={5} tip={<Text style={{ color: '#fff', marginTop: 15 }}>Verify login...</Text>} />
+        </Flex>
+        <Footer style={{ marginBottom: 20 }} />
+      </Flex>
+    </SafeAreaView>
+  );
 }
 
 export default connect(
-  ({global, loading}) => ({
+  ({ global }) => ({
     token: global.token,
     authState: global.authState,
-    loading: loading.effects.global.authToken,
   }),
-  ({global}) => ({
-    updateState: global.update,
-    authToken: global.authToken,
-  }),
+  ({ global }) => ({
+    update: global.update,
+  })
 )(AuthLoadingScreen);
 
 const styles = StyleSheet.create({
--- template/template/src/models/global.js
+++ template/template/src/models/global.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {userAuth} from '../services/users';
+import { userAuth } from '../services/users';
 import conf from '../config';
 
 export default {
@@ -10,29 +10,10 @@ export default {
     authState: false,
     token: null,
     apihost: null,
+    userData: null,
   },
   reducers: {
-    update: (state, payload) => ({...state, ...payload}),
+    update: (state, payload) => ({ ...state, ...payload }),
   },
-  effects: dispatch => ({
-    async authToken(_, {global}) {
-      let host = await AsyncStorage.getItem('apihost');
-      if (!host && conf.hosts[0]) {
-        await AsyncStorage.setItem('apihost', JSON.stringify(conf.hosts[0]));
-        await this.update({apihost: conf.hosts[0]});
-      }
-
-      if (!global.token) {
-        await AsyncStorage.removeItem('userData');
-        await AsyncStorage.removeItem('token');
-      }
-
-      const data = await userAuth();
-      if (data && data.token) {
-        await this.update({authState: true, token: data.token});
-      } else {
-        await this.update({authState: true, token: null});
-      }
-    },
-  }),
+  effects: dispatch => ({}),
 };
