fe038537dd602e6ab687147173c47f2ddc7ee2bc
WX <42308734@qq.com>
2023-03-03 09:19:22 +0800

    fix: 增加eslint配置

--- template/template/_eslintrc.js
+++ template/template/_eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-shadow': 'off',
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
 };
